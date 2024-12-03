02f55e0f6cfb09bfd352692ea130c30a8bc77ddc
Alba  Gomez <albaliriagomezmontano0419@gmail.com>
2024-12-03 10:17:09 -0400

    Se implemento pantalla de guia y recursos

--- app/src/main/java/com/torrezpillcokevin/nuna/ui/guia/GuiaFragment.kt
+++ app/src/main/java/com/torrezpillcokevin/nuna/ui/guia/GuiaFragment.kt
@@ -7,31 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.torrezpillcokevin.nuna.R
 
 class GuiaFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = GuiaFragment()
-    }
-
-    private val viewModel: GuiaViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.fragment_guia, container, false)
 
         // Configuración del botón "Volver al Centro de Ayuda"
         val backToHelpCenter = view.findViewById<TextView>(R.id.backToHelpCenter)
         backToHelpCenter.setOnClickListener {
-            // Navegar al centro de ayuda (o implementar lógica de navegación personalizada)
             requireActivity().onBackPressed()
         }
 
@@ -46,14 +34,12 @@ class GuiaFragment : Fragment() {
             navigateToDetails("Start using the app")
         }
 
-        // Continúa configurando clics para los demás elementos...
-
         return view
     }
 
     private fun navigateToDetails(title: String) {
-        //val intent = Intent(requireContext(), GuiaDetailActivity::class.java)
-        //intent.putExtra("TITLE", title)
-        //startActivity(intent)
+        val intent = Intent(requireContext(), GuiaViewModel::class.java)
+        intent.putExtra("TITLE", title)
+        startActivity(intent)
     }
 }
