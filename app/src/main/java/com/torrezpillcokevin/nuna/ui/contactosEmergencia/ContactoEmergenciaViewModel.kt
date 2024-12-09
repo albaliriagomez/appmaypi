7618382cbc7641282ef3229e1adfec7c2b79cdbd
Kevin Torrez Pillco <cbbe.kevin.torrez.pi@unifranz.edu.bo>
2024-12-08 23:28:40 -0400

    Se añadio 3 Fragmentos (Reportar,Contactos Emergencia y Codigo Rastreo)

---
+++ app/src/main/java/com/torrezpillcokevin/nuna/ui/contactosEmergencia/ContactoEmergenciaViewModel.kt
@@ -0,0 +1,7 @@
+package com.torrezpillcokevin.nuna.ui.contactosEmergencia
+
+import androidx.lifecycle.ViewModel
+
+class ContactoEmergenciaViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
