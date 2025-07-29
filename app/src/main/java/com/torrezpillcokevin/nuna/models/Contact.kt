0c09f734b14355e53446ae7feee4fd0fb5eb3574
Kevin Torrez Pillco <cbbe.kevin.torrez.pi@unifranz.edu.bo>
2025-07-28 23:07:31 -0400

    configuro y dio funcionalidad fragmento contacto de emergencia

--- app/src/main/java/com/torrezpillcokevin/nuna/models/Contact.kt
+++ app/src/main/java/com/torrezpillcokevin/nuna/models/Contact.kt
@@ -1,8 +1,10 @@
 package com.torrezpillcokevin.nuna.models
 
+import java.io.Serializable
+
 data class Contact(
     val id: Int = 0,
     val name: String,
     val phone: String,
     val line: String
-)
+) : Serializable
